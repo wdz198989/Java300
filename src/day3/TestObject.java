0b323f8d4769c4184e7e01d2cccd3dd26d80a87d
wudazhong <wdz798539582@qq.com>
2019-07-03 17:51:22 +0800

    第三天测试

---
+++ src/day3/TestObject.java
@@ -0,0 +1,44 @@
+package day3;
+
+
+/**
+ * @Author : WuDazhong
+ * @Date :2019/7/3 16:17
+ * @Description : ≤‚ ‘object
+ */
+public class TestObject {
+    public static void main(String[] args) {
+
+        TestObject   to = new TestObject();
+        System.out.println(to.toString());
+
+        Person2  p2 = new Person2("Œ˚Œ˚", 6);
+        System.out.println(p2.toString());
+        Object obj;
+        String str;
+
+    }
+
+    public String toString(){
+        return  "≤‚ ‘Object∂‘œÛ";
+    }
+
+}
+
+
+class Person2 {
+    String name;
+    int age;
+
+    @Override
+    public String toString() {
+        return name+",ƒÍ¡‰"+age;
+    }
+
+    public Person2(String  name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+}
