b259b89847974881958837b5c5fafd748829dd5e
wudazhong <17512570148@163.com>
2020-03-16 19:22:54 +0800

    静态代理

---
+++ src/DesignPattern/staticProxy/ITeacherDao.java
@@ -0,0 +1,6 @@
+package DesignPattern.staticProxy;
+// 接口
+public interface ITeacherDao {
+    void teach();// 授课方法
+    void say();
+}
---
+++ src/DesignPattern/staticProxy/TeacherDao.java
@@ -0,0 +1,13 @@
+package DesignPattern.staticProxy;
+
+public class TeacherDao  implements ITeacherDao{
+    @Override
+    public void teach() {
+        System.out.println("老师正在授课...........");
+    }
+
+    @Override
+    public void say() {
+        System.out.println("老师在说话 .................");
+    }
+}
